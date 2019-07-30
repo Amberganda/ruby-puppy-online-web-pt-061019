9753f8c8deca4669b718b4e9bb9a9d4458cf6273
Amber Taranto <ambzzzz@gmail.com>
2019-07-30 02:12:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+#        @@all << self #object which is an instance of dog class
+        self.save
+    end
+
+
+    def self.all #class method
+        @@all
+    end
+
+    def self.clear_all
+#        @@all = [] one way works
+        @@all.clear #another way works
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+
+    def save
+        @@all << self
+    end
+
+
+end
