f1b9c22ff9cce31b86b35da3166a1ed5b1a02408
Amber Taranto <ambzzzz@gmail.com>
2019-07-30 01:51:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
-    def initialize(name)
-        @name = name
-#        dog << self
-    end
-
 
     @@all = []
 
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
     def self.all
-        
+        @@all
     end
 
 
